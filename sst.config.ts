97ed8128ef3194e04bf5d3f3cb14b7bf43181ccf
zorro901 <zoro907@gmail.com>
2023-07-30 15:31:39 +0900

    setup github actions

--- sst.config.ts
+++ sst.config.ts
@@ -1,4 +1,4 @@
-import { SSTConfig } from "sst";
+import { type SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 
 export default {
@@ -10,7 +10,11 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "site", {
+        environment: {
+          DATABASE_URL: process.env.DATABASE_URL ?? "",
+        },
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
