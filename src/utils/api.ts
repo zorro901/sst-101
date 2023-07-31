22162d8ebfdb2276088fc10f706831676153210e
zorro901 <zoro907@gmail.com>
2023-07-31 20:57:43 +0900

    add custom domain

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -10,12 +10,6 @@ import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import superjson from "superjson";
 import { type AppRouter } from "~/server/api/root";
 
-const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-};
-
 /** A set of type-safe react-query hooks for your tRPC API. */
 export const api = createTRPCNext<AppRouter>({
   config() {
@@ -39,7 +33,7 @@ export const api = createTRPCNext<AppRouter>({
             (opts.direction === "down" && opts.result instanceof Error),
         }),
         httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
+          url: `https://robavo.net/api/trpc`,
         }),
       ],
     };
--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,6 @@
 import { type SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
 
 export default {
   config(_input) {
@@ -10,9 +11,22 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
+      const certificate = acm.Certificate.fromCertificateArn(
+        stack,
+        "Certificate",
+        process.env.AWS_CERTIFICATE_ARN ?? "",
+      );
       const site = new NextjsSite(stack, "site", {
         environment: {
           DATABASE_URL: process.env.DATABASE_URL!,
+        },
+        customDomain: {
+          isExternalDomain: true,
+          domainName: "www.robavo.net",
+          alternateNames: ["robavo.net"],
+          cdk: {
+            certificate,
+          },
         },
       });
 
