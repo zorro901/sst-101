2a38b997779491fcf280e16b10fbcbedcf213485
zorro901 <zoro907@gmail.com>
2023-07-31 14:52:05 +0900

    aws-cdk-libを2.84.0

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,6 @@
 import { type SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager/lib/certificate";
 
 export default {
   config(_input) {
@@ -10,12 +11,23 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
+      const certificate = Certificate.fromCertificateArn(
+        stack,
+        "Certificate",
+        process.env.AWS_CERTIFICATE_ARN ?? "",
+      );
+
       // Create site
       const site = new NextjsSite(stack, "Site", {
         environment: {
           DATABASE_URL: process.env.DATABASE_URL ?? "",
         },
-        customDomain: "robavo.net",
+        customDomain: {
+          domainName: "robavo.net",
+          cdk: {
+            certificate,
+          },
+        },
       });
       stack.addOutputs({
         SiteUrl: site.url,
