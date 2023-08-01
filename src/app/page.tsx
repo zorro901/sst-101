a6997b8240c80b04c8823c3cd410dc056594e189
zorro901 <zoro907@gmail.com>
2023-08-01 12:57:07 +0900

    edge test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,19 @@
-import { createTRPCCaller } from "~/utils/api-rsc";
-import { HomePage } from "./home";
-import { getServerSession } from "next-auth";
-import { authOptions } from "~/server/auth";
-
-export default async function Home() {
-  const session = await getServerSession(authOptions);
-  const caller = await createTRPCCaller(session);
-  const hello = await caller.example.hello({
-    text: "Trpc from server component",
-  });
-  const secretMessage = session
-    ? await caller.example.getSecretMessage()
-    : undefined;
-
-  return (
-    <HomePage
-      hello={hello}
-      secretMessage={secretMessage}
-      serverSession={session}
-    />
-  );
+export default function Home() {
+  return <p className={"text-white"}>test</p>;
+  // const session = await getServerSession(authOptions);
+  // const caller = await createTRPCCaller(session);
+  // const hello = await caller.example.hello({
+  //   text: "Trpc from server component",
+  // });
+  // const secretMessage = session
+  //   ? await caller.example.getSecretMessage()
+  //   : undefined;
+  //
+  // return (
+  //   <HomePage
+  //     hello={hello}
+  //     secretMessage={secretMessage}
+  //     serverSession={session}
+  //   />
+  // );
 }
